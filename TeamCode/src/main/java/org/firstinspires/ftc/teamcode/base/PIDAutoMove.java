02619a0d96e50a0bd610c1bef9fb9ff7db972d41
unknown <ooperloops@gmail.com>
2024-11-05 12:30:24 -0700

    Added PID functions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/PIDAutoMove.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.base;
+
+
+public class PIDAutoMove {
+    public double Ki = 5.0;
+
+
+    public PIDAutoMove(){
+
+    }
+
+    public void OnUpdate(double Theta){
+
+    }
+
+    public double Integral(){
+
+    }
+
+    public double Derivative(){
+
+    }
+
+    public double Proportion(){
+
+    }
+}
