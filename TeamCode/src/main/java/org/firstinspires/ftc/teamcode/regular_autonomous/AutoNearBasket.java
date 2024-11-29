137b63a7abf4ad7521cc9dfcecaba114c8e4a63c
unknown <ooperloops@gmail.com>
2024-11-29 15:38:17 -0700

    Changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/regular_autonomous/AutoNearBasket.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/regular_autonomous/AutoNearBasket.java
@@ -12,16 +12,17 @@ public class AutoNearBasket extends SelfDriving {
      */
     @Override
     public void runAutonomous() {
-        MoveWheel(false, 2);
+        hardwareManager.intakeServo.setPosition(0);
         move(0.2);
         rotate(-90);
         move(2);
         rotate(135);
         MoveUpwardSlide(true);
         MoveScoreBasket();
+        Delay(2000);
         MoveUpwardSlide(false);
         rotate(45);
-        move(0.77);
+        move(2);
         rotate(-90);
     }
 }
