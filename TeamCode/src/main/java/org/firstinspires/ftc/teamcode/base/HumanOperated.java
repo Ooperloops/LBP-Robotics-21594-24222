22d3723b9555366adee03929b0d5fadf813d408b
unknown <ooperloops@gmail.com>
2025-02-23 11:45:21 -0700

    Fixed Driving

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/HumanOperated.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/HumanOperated.java
@@ -130,8 +130,8 @@ public abstract class HumanOperated extends OpMode {
         double strafe = gamepad1.left_stick_x;
         double rotate = gamepad1.right_stick_x;
 
-        frontLeftWheelP  = drive + strafe + rotate;
-        frontRightWheelP = drive - strafe - rotate;
+        frontLeftWheelP  = -drive + strafe + rotate;
+        frontRightWheelP = -drive - strafe - rotate;
         backLeftWheelP   = drive - strafe + rotate;
         backRightWheelP  = drive + strafe - rotate;
     }
