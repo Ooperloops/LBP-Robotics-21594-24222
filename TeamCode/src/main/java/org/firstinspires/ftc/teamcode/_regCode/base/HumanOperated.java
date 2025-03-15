928537780ea2a0cbdc5808119683e66192b249ea
OoperLoops <ooperloops@gmail.com>
2025-03-15 09:12:44 -0600

    Fixed Ang claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_regCode/base/HumanOperated.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_regCode/base/HumanOperated.java
@@ -159,10 +159,10 @@ public abstract class HumanOperated extends OpMode {
 
 
         // Rotational Wrist Servo Control
-        if(gamepad2.right_bumper  && AngClawPos > 0){
-            AngClawPos = AngClawPos - 0.125;
-        } else if (gamepad2.left_bumper ){
-            AngClawPos = AngClawPos + 0.125;
+        if(gamepad2.right_bumper){
+            AngClawPos = Range.clip(AngClawPos + increment, 0, 1);
+        } else if (gamepad2.left_bumper){
+            AngClawPos = Range.clip(AngClawPos - increment, 0, 1);
         }
 
         // Claw Control
